3055db9bdd42f500ea0637ea43ef45239ef7b1cc
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-09-19 15:24:34 -0300

    Client Side Login and Register
     - Created Login and Register Components with Angular Services
     - Implemented Forms with Angular Template Forms

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  model: any = {}; // Store username and password
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+        console.log('Logged in successfully!');
+    }, error => {
+        console.log('Failed to login due to error ', error);
+    })
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token; // same as return token != null
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
+
+}
---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(): void {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  cancel(): void {
+    this.cancelRegister.emit(false); // Can be anything; in this case it's false
+    console.log('Cancelled...');
+  }
+}
