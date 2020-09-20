7c4bfc036466121e94fca7053cf0dc1f8fe399dd
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-09-20 17:03:37 -0300

    Extending the API
     - More data in User Class
     - More migrations
     - Cascade delete
     - Seeding data to mass populate SQLite
     - Using Entity Framework Migrations and Automapper

--- DatingApp.API/DTOs/UserForDetailedDTO.cs
+++ DatingApp.API/DTOs/UserForDetailedDTO.cs
@@ -1,21 +1,20 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.DTOs
 {
-    public class User
+    public class UserForDetailedDTO
     {
         public int Id { get; set; }
 
         public string Username { get; set; }
 
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+
+        public DateTime Created { get; set; }
 
         public DateTime LastActive { get; set; }
 
@@ -29,6 +28,6 @@ namespace DatingApp.API.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos {get; set;}
+        public string PhotoUrl { get; set; }
     }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -35,9 +36,14 @@ namespace DatingApp.API
         {
             // ORDER DOES NOT MATTER
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(opt => {
+                opt.SerializerSettings.ReferenceLoopHandling = 
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddCors();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>(); // Telling the system about the Auth Service
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
